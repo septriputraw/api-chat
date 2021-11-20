4c9a15894d324e19f989b42c2bdb3699125bc20f
tra <09021381621129@students.ilkom.unsri.ac.id>
2021-11-20 17:44:42 +0700

    user plugin implemented

--- src/server.js
+++ src/server.js
@@ -3,37 +3,13 @@ require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const ClientError = require('./exceptions/ClientError');
 
-const barang = require('./api/barang');
-const BarangService = require('./services/inMemory/BarangService');
-const BarangValidator = require('./validator/barang');
-
-const item = require('./api/items');
-const ItemsService = require('./services/postgres/ItemsService');
-const ItemValidator = require('./validator/item');
-
-const transaksi = require('./api/transaksi');
-const TransaksiService = require('./services/inMemory/TransaksiService');
-
-const transaction = require('./api/transactions');
-const TransactionValidator = require('./validator/transaction');
-const TransactionsService = require('./services/postgres/TransactionsService');
-
 // users
 const users = require('./api/users');
 const UsersService = require('./services/postgres/UsersService');
 const UsersValidator = require('./validator/users');
 
-const chat = require('./api/chat');
-const ChatService = require('./services/postgres/chatService');
-const ChatValidator = require('./validator/chat');
-
 const init = async () => {
-  const barangService = new BarangService();
-  const transaksiService = new TransaksiService();
-  const itemsService = new ItemsService();
-  const transactionsService = new TransactionsService();
   const usersService = new UsersService();
-  const chatService = new ChatService();
 
   const server = Hapi.Server({
     port: process.env.PORT,
@@ -46,42 +22,6 @@ const init = async () => {
   });
 
   await server.register([
-    {
-      plugin: barang,
-      options: {
-        service: barangService,
-        validator: BarangValidator,
-      },
-    },
-    {
-      plugin: transaksi,
-      options: {
-        service: transaksiService,
-        serviceBarang: barangService,
-      },
-    },
-    {
-      plugin: item,
-      options: {
-        service: itemsService,
-        validator: ItemValidator,
-      },
-    },
-    {
-      plugin: transaction,
-      options: {
-        service: transactionsService,
-        serviceItem: itemsService,
-        validator: TransactionValidator,
-      },
-    },
-    {
-      plugin: chat,
-      options: {
-        service: chatService,
-        validator: ChatValidator,
-      },
-    },
     {
       plugin: users,
       options: {
